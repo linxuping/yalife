6bbb7a4bf2a764717a2456b5a4efd2cc9671ae33
linxuping <linxuping@126.com>
2019-09-19 19:38:44 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -13,12 +13,13 @@ App({
     this.globalData = {}
     this.getOpenid()
   },//获取用户地理位置权限
-  getPermission: function (obj) {
+  chooseLocation: function (cb) {
     wx.chooseLocation({
       success: function (res) {
-        obj.setData({
+        /*obj.setData({
           addr: res.address      //调用成功直接设置地址
-        })
+        })*/
+        cb(res);
       },
       fail: function () {
         wx.getSetting({
@@ -39,18 +40,11 @@ App({
                             duration: 1000
                           })
                           //授权成功之后，再调用chooseLocation选择地方
-                          /*wx.chooseLocation({
+                          wx.chooseLocation({
                             success: function (res) {
-                              obj.setData({
-                                addr: res.address
-                              })
-                            },
-                          })*/
-                          wx.getLocation({
-                            success: function(res) {
+                              cb(res);
                             },
                           })
-              
                         } else {
                           wx.showToast({
                             title: '授权失败',
@@ -75,7 +69,62 @@ App({
         })
       }
     })
-  },saveLocation() {
+  },
+  getLocation: function (cb) {
+    wx.getLocation({
+      success: function (res) {
+        cb(res);
+      },
+      fail: function () {
+        wx.getSetting({
+          success: function (res) {
+            var statu = res.authSetting;
+            if (!statu['scope.userLocation']) {
+              wx.showModal({
+                title: '是否授权当前位置',
+                content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                success: function (tip) {
+                  if (tip.confirm) {
+                    wx.openSetting({
+                      success: function (data) {
+                        if (data.authSetting["scope.userLocation"] === true) {
+                          wx.showToast({
+                            title: '授权成功',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                          //授权成功之后，再调用chooseLocation选择地方
+                          wx.getLocation({
+                            success: function (res) {
+                              cb(res);
+                            },
+                          })
+                        } else {
+                          wx.showToast({
+                            title: '授权失败',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '调用授权窗口失败',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        })
+      }
+    })
+  },
+  saveLocation() {
     var page = this;
     var db = wx.cloud.database();
     db.collection('user_location').where(
