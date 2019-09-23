857a42c9954902b13aac59c6c07a1396a439657d
linxuping <linxuping@126.com>
2019-09-23 19:04:08 +0800

    Update comment.js

--- miniprogram/utils/comment.js
+++ miniprogram/utils/comment.js
@@ -1,10 +1,42 @@
+
+function formatDate(time) {
+  var date = new Date(time);
+  var year = date.getFullYear(),
+    month = date.getMonth() + 1,//月份是从0开始的
+    day = date.getDate(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+  var newTime = year + '-' +
+    (month < 10 ? '0' + month : month) + '-' +
+    (day < 10 ? '0' + day : day) + ' ' +
+    (hour < 10 ? '0' + hour : hour) + ':' +
+    (min < 10 ? '0' + min : min) + ':' +
+    (sec < 10 ? '0' + sec : sec);
+  return newTime;
+}
+
+
 class Comment {
-  /**
-   * 登陆
-   * @return {Promise} 
-   */
-  static login() {
-    return new Promise((resolve, reject) => wx.login({ success: resolve, fail: reject }));
+  static add(cardId, content) {
+    db.collection('comment').add({
+      data: {
+        card_id: cardId,
+        content: content,
+        status: 2,
+        create_time: formatDate(new Date().getTime())
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(console.error) 
+  };
+  
+  static remove(cardId) {
+    db.collection('comment').doc(cardId).update({
+      data: {
+        status: 0
+      }
+    }) 
   };
 
   /**
