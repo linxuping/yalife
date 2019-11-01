8e524fa2608b8c40e4719ec2b6f64d8de2907aa7
linxuping <linxuping@126.com>
2019-11-01 17:59:17 +0800

    Update editCard.js

--- miniprogram/pages/editCard/editCard.js
+++ miniprogram/pages/editCard/editCard.js
@@ -248,6 +248,9 @@ Page({
       console.log(`rejected: ${value}`); // 'rejected: Hello World'
     });
   },
+  onBought: function () {
+    var page = this;
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
