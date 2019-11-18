6c019b37bac2fc6f64c93f098a5712a58977e5da
linxuping <linxuping@126.com>
2019-11-18 19:26:43 +0800

    Update genCard.js

--- miniprogram/pages/genCard/genCard.js
+++ miniprogram/pages/genCard/genCard.js
@@ -74,6 +74,26 @@ Page({
       }
     })*/
 
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+              var userInfo = res.userInfo;
+              var gender = userInfo.gender;
+              console.log(gender);
+              that.setData({
+                userInfo: userInfo,
+                gender: gender
+              })
+            }
+          })
+        }
+      }
+    });
+
   },
   onShow: function (options) {
     console.log("onshow.");
